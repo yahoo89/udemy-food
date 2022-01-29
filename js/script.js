89c370493eeb4a6520ec24cab60f67508b2ad17e
Tigran <tigran2589@gmail.com>
2022-01-29 13:52:03 +0200

    Finish to work with XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -241,15 +241,28 @@ window.addEventListener('DOMContentLoaded', () => {
       const request = new XMLHttpRequest();
       request.open('POST', 'server.php');
 
-      request.setRequestHeader('Content-type', 'multipart/form-data');
+      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
-      request.send(formData);
+      const object = {};
+      formData.forEach((value, key) => {
+        object[key] = value;
+      });
+
+      console.dir(object);
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
 
       request.addEventListener('load', () => {
         if (request.status === 200) {
           console.log(request.response);
           statusMessage.textContent = messeges.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
         } else {
           statusMessage.textContent = messeges.failure;
         }
